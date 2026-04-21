c454e3f5180c975546c44cbde703609178fc6238
Jeya Stephen <jeyastephen025@gmail.com>
2026-04-21 15:04:45 +0530

    Update wsgi.py

--- internx_backend/internx/wsgi.py
+++ internx_backend/internx/wsgi.py
@@ -4,5 +4,5 @@ WSGI config for InternX project.
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internx.settings')
 application = get_wsgi_application()
