60e55d3f3f86e64c6d88181ec88e69fef94fbb4a
Jeya Stephen <jeyastephen025@gmail.com>
2026-04-27 15:58:31 +0530

    new commit

--- internx_backend/internx/settings.py
+++ internx_backend/internx/settings.py
@@ -22,6 +22,14 @@ CSRF_TRUSTED_ORIGINS = [
     "https://learning-platform-beta-one.vercel.app",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    ...
+]
 # Build paths
 BASE_DIR = Path(__file__).resolve().parent.parent
 
